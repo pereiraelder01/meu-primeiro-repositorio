80d6bda5c9d97a41a8e55f329bcac6261f2a4ef9
ELDER MAX SILVA PEREIRA <x00719715130@tcu.gov.br>
2025-03-28 15:06:22 -0300

    adicionando log jogos

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("rodando sistema de gerenciamento de jogos")
 console.log("rodando sistema de gerenciamento de jogos e Apostas")
 console.log("Enviando apostas")
+console.log("Enviando jogos")
